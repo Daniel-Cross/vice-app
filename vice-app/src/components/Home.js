652351fac83465dfe5b273a7619a7301213b956f
Daniel-Cross <crossy1686@gmail.com>
2018-10-09 19:36:47 +0100

    home component

---
+++ vice-app/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/Home.css';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div className="Home">APP FUNCTION HERE</div>;
+  }
+}
+
+export default Home;
---
+++ vice-app/src/components/Logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/Logo.css';
+
+const Logo = () => {
+  return (
+    <div className="Logo">
+      <div className="logo">LOGO</div>
+      <div className="tagline">hello</div>
+    </div>
+  );
+};
+
+export default Logo;
