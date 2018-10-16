e121d7743650ef15b9d575cadcca13b3b3f9bf80
Daniel-Cross <crossy1686@gmail.com>
2018-10-16 11:04:29 +0100

    Updates

---
+++ precache-manifest.744d318ff1b3a62d4906e51253e92c58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05e39d9231d4b0f18267",
+    "url": "/static/js/main.05e39d92.chunk.js"
+  },
+  {
+    "revision": "4defdff3e64088171cca",
+    "url": "/static/js/1.4defdff3.chunk.js"
+  },
+  {
+    "revision": "05e39d9231d4b0f18267",
+    "url": "/static/css/main.abf70cfb.chunk.css"
+  },
+  {
+    "revision": "a4195c132b0d4213d6e9820ed7d51056",
+    "url": "/index.html"
+  }
+];
